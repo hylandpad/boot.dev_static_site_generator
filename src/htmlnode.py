eb77d7f5b89b7b733c4cea82ce0492cc1ae5a2fa
hylandpad <hylandpad@gmail.com>
2025-12-16 01:32:44 -0500

    htmlnode changes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,8 +22,39 @@ class LeafNode(HTMLNode):
         super().__init__(tag,value,None,props)
         
     def to_html(self):
+        if self.value is None:
+           raise ValueError("LeafNode requires a value to create HTML.")
+       
+        if self.tag is None:
+           return self.value
+        
         if self.props:
             prop_string = self.props_to_html()
             return f'<{self.tag} {prop_string}>{self.value}</{self.tag}>'
         return f'<{self.tag}>{self.value}</{self.tag}>'
+    
+    
+class ParentNode(HTMLNode):
+    def __init__(self,tag,children,props=None):
+        super().__init__(tag,None,children,props)
+        
+    def to_html(self):
+        if self.tag is None:
+           raise ValueError("ParentNode requires a tag to create HTML.")
+       
+        if self.children is None:
+           raise ValueError("ParentNode requires at least one child to create HTML.")
+        
+        if self.children == []:
+            child_str = None
+        else: 
+            child_str = ''
+            for child in self.children:
+                child_str += child.to_html()
+        
+        if self.props:
+            prop_string = self.props_to_html()
+            html_str = f'<{self.tag}{prop_string if prop_string else ""}>{child_str if child_str else ""}</{self.tag}>'
+
+        return html_str 
     
