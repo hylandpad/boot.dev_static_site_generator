f2e6983dd96fdf7b6f707dc99a35ab3f2e48c038
hylandpad <hylandpad@gmail.com>
2025-12-18 16:52:57 -0500

    text node to html node function added

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import ParentNode
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = 'text'
@@ -21,3 +23,19 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type.TEXT:
+        return LeafNode(None,text_node.value)
+    elif text_node.text_type.BOLD:
+        return LeafNode('b',text_node.value)
+    elif text_node.text_type.ITALIC:
+        return LeafNode('i',text_node.value)
+    elif text_node.text_type.CODE:
+        return LeafNode('code',text_node.value)
+    elif text_node.text_type.LINK:
+        return LeafNode('a',text_node.value)
+    elif text_node.text_type.IMAGE:
+        return LeafNode('img',text_node.value)
+    else:
+        raise Exception("Text Node text not of valid type")
